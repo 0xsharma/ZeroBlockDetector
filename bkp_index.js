7762f3d4408dff91c336748520f57243bbc8235d
Shivam Sharma <shivam691999@gmail.com>
2022-01-07 15:34:50 +0530

    Added gas stats

--- bkp_index.js
+++ bkp_index.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 const Web3 = require('web3');
 const fs = require('fs')
 
-var WSWEB3 = WEB3_WEBSOCKET_URL
+var WSWEB3 = 'wss://polygon-mainnet.g.alchemy.com/v2/sstrrWqziLhH3KWlbOYheMVQgrnqtLrQ'
 var web3 = new Web3(Web3.givenProvider || WSWEB3);
 
 const timer = ms => new Promise(res => setTimeout(res, ms))
@@ -23,6 +23,7 @@ async function getBlockTxCount(lastStartingTime){
             blockNumber : blockNum,
             timestamp : block.timestamp,
             txCount : block.transactions.length,
+            gas : block.gasUsed,
             blockMiner : blockMiner,
         }
     
@@ -37,7 +38,7 @@ async function getBlockTxCount(lastStartingTime){
 
 async function getBlockValidator(blockNum){
     var blockMiner
-    var APIKEY = POLYGONSCAN-API-KEY
+    var APIKEY = 'B6B5CR6BDBSYFRQA177D8F83WEK2H796MF'
     await axios.post(`https://api.polygonscan.com/api?module=block&action=getblockreward&blockno=${blockNum}&apikey=${APIKEY}`,{})
     .then((response)=>{
         blockMiner = response.data.result.blockMiner
@@ -54,7 +55,7 @@ async function iteration(lastStartingTime){
     var validators = {}
     var totalBlocksPerIteration = 0
     var totalEmptyBlocksPerIteration = 0
-    while ((Math.floor(new Date().getTime() / 1000))-lastStartingTime <= 600){
+    while ((Math.floor(new Date().getTime() / 1000))-lastStartingTime <= 20){
         var blockS = await getBlockTxCount(lastStartingTime)
         if(blockS!==undefined){
 
@@ -62,13 +63,27 @@ async function iteration(lastStartingTime){
                 
                 var blockMinerDetails = {
                     totalBlockCount : 0,
-                    emptyBlocksCount : 0
+                    emptyBlocksCount : 0,
+                    minGas : 9999999999999,
+                    maxGas : 0,
+                    avgGas : 0,
                 }
+
                 totalBlocks +=1
                 blockMinerDetails.totalBlockCount += 1
+                
                 if(blockS.txCount===0){ 
                     totalEmptyBlocks+=1
                     blockMinerDetails.emptyBlocksCount +=1
+                }else{
+                    var nonEmptyBlocks = blockMinerDetails.totalBlockCount-blockMinerDetails.emptyBlocksCount
+                    blockMinerDetails.avgGas = (blockMinerDetails.avgGas*(nonEmptyBlocks-1) + blockS.gas)/nonEmptyBlocks
+                    if(blockS.gas>blockMinerDetails.maxGas){
+                        blockMinerDetails.maxGas = blockS.gas
+                    }
+                    if(blockS.gas<blockMinerDetails.minGas){
+                        blockMinerDetails.minGas = blockS.gas
+                    }
                 }
                 validatorsLegacy[blockS.blockMiner] = blockMinerDetails
 
@@ -79,21 +94,42 @@ async function iteration(lastStartingTime){
                 if(blockS.txCount===0){
                     totalEmptyBlocks+=1
                     validatorsLegacy[blockS.blockMiner].emptyBlocksCount +=1
+                }else{
+                    var nonEmptyBlocks = validatorsLegacy[blockS.blockMiner].totalBlockCount-validatorsLegacy[blockS.blockMiner].emptyBlocksCount
+                    validatorsLegacy[blockS.blockMiner].avgGas = (validatorsLegacy[blockS.blockMiner].avgGas*(nonEmptyBlocks-1) +blockS.gas)/nonEmptyBlocks
+                    if(blockS.gas>validatorsLegacy[blockS.blockMiner].maxGas){
+                        validatorsLegacy[blockS.blockMiner].maxGas = blockS.gas
+                    }
+                    if(blockS.gas<validatorsLegacy[blockS.blockMiner].minGas){
+                        validatorsLegacy[blockS.blockMiner].minGas = blockS.gas
+                    }
                 }
-
             }
 
             if(validators[blockS.blockMiner]===undefined){
 
                 var blockMinerDetails = {
                     totalBlockCount : 0,
-                    emptyBlocksCount : 0
+                    emptyBlocksCount : 0,
+                    minGas : 9999999999999,
+                    maxGas : 0,
+                    avgGas : 0,
                 }
+
                 totalBlocksPerIteration += 1
                 blockMinerDetails.totalBlockCount += 1
                 if(blockS.txCount===0){
                     totalEmptyBlocksPerIteration += 1
                     blockMinerDetails.emptyBlocksCount +=1
+                }else{
+                    var nonEmptyBlocks = blockMinerDetails.totalBlockCount-blockMinerDetails.emptyBlocksCount
+                    blockMinerDetails.avgGas = (blockMinerDetails.avgGas*(nonEmptyBlocks-1) +blockS.gas)/nonEmptyBlocks
+                    if(blockS.gas>blockMinerDetails.maxGas){
+                        blockMinerDetails.maxGas = blockS.gas
+                    }
+                    if(blockS.gas<blockMinerDetails.minGas){
+                        blockMinerDetails.minGas = blockS.gas
+                    }
                 }
                 validators[blockS.blockMiner] = blockMinerDetails
 
@@ -104,6 +140,15 @@ async function iteration(lastStartingTime){
                 if(blockS.txCount===0){
                     totalEmptyBlocksPerIteration += 1
                     validators[blockS.blockMiner].emptyBlocksCount +=1
+                }else{
+                    var nonEmptyBlocks = validators[blockS.blockMiner].totalBlockCount-validators[blockS.blockMiner].emptyBlocksCount
+                    validators[blockS.blockMiner].avgGas = (validators[blockS.blockMiner].avgGas*(nonEmptyBlocks-1) +blockS.gas)/nonEmptyBlocks
+                    if(blockS.gas>validators[blockS.blockMiner].maxGas){
+                        validators[blockS.blockMiner].maxGas = blockS.gas
+                    }
+                    if(blockS.gas<validators[blockS.blockMiner].minGas){
+                        validators[blockS.blockMiner].minGas = blockS.gas
+                    }
                 }
 
             }
